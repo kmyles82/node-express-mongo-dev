2963d03add72512a77fb4edc6c6ff47576f91855
kmyles82 <kmyles82@gmail.com>
2020-11-10 14:37:53 -0600

    setup user registration

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const passPort = require('passport')
+
+//Load User model
+require('../models/User')
+const User = mongoose.model('users')
+
 
 //Connect to mongoose
 mongoose.connect('mongodb://localhost/vidjot-dev', {
@@ -49,10 +56,48 @@ router.post('/register', (req, res) => {
             password2: req.body.password2
         })
     } else {
-        res.send('Passed')
+
+        User.findOne({email: req.body.email})
+        .then(user => {
+            if(user){
+                req.flash('error_msg', 'Email already registered')
+                res.redirect('/users/register')
+            }else {
+                const newUser = new User({
+                    name: req.body.name,
+                    email: req.body.email,
+                    password: req.body.password
+                })
+        
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+        
+                        newUser.password = hash;
+        
+                        newUser.save()
+                        .then(user => {
+                            req.flash('success_msg', 'You are now registered and can login')
+                            res.redirect('/users/login')
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            return
+                        }) 
+                    })
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err)
+        }) 
+
+        
+
+        // console.log(newUser)
     }
 
-    console.log(errors)
+    // console.log(errors)
 })
 
 module.exports = router
