3f03df6da16b6e752d11f1baa753be94bfde5433
Kerry L. Myles <kmyles@mdd.com>
2020-11-20 21:39:24 -0600

    protecting routes functionality added

--- vidjot/routes/ideas.js
+++ vidjot/routes/ideas.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const router = express.Router()
+const { ensureAuthenticated } = require('../helpers/auth')
 
 //Load Idea Model
 require('../models/Idea')
 const Idea = mongoose.model('ideas')
 
 //Idea Index Page
-router.get('/', (req, res) => {
+router.get('/', ensureAuthenticated, (req, res) => {
     Idea.find({})
     .sort({date: 'desc'}).lean()
     .then(ideas => {
@@ -19,12 +20,12 @@ router.get('/', (req, res) => {
 })
 
 //Add Idea form
-router.get('/add', (req, res) => {
+router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('ideas/add')
 })
 
 //Edit Idea form
-router.get('/edit/:id', (req, res) => {
+router.get('/edit/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({ 
         _id: req.params.id
     })
@@ -38,7 +39,7 @@ router.get('/edit/:id', (req, res) => {
 })
 
 //Process Form
-router.post('/', (req, res) => {
+router.post('/', ensureAuthenticated, (req, res) => {
     let errors = []
 
     if(!req.body.title){
@@ -73,7 +74,7 @@ router.post('/', (req, res) => {
 })
 
 //Edit From process
-router.put('/:id', (req, res) => {
+router.put('/:id', ensureAuthenticated, (req, res) => {
     Idea.findOne({
         _id: req.params.id
     })
@@ -91,7 +92,7 @@ router.put('/:id', (req, res) => {
 })
 
 //Delete Idea
-router.delete('/:id', (req, res) => {
+router.delete('/:id', ensureAuthenticated, (req, res) => {
     Idea.remove({
         _id: req.params.id
     })
